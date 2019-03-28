aa81f89d07246f7c252ffc2bd9c3369980630b6e
gianl <gianl@LAPTOP-9HUNOSBE>
2019-03-28 08:58:31 +0100

    added isInteger()

--- karteikarten/src/main/java/de/kksystem/karteikarten/utils/StringUtils.java
+++ karteikarten/src/main/java/de/kksystem/karteikarten/utils/StringUtils.java
@@ -1,5 +1,28 @@
 package de.kksystem.karteikarten.utils;
 
 public class StringUtils {
-
+	// Überprüfe, ob tatsächlich ein Integer
+	public static boolean isInteger(String str) {
+	    if (str == null) {
+	        return false;
+	    }
+	    if (str.isEmpty()) {
+	        return false;
+	    }
+	    int i = 0;
+	    if (str.charAt(0) == '-') {
+	        if (str.length() == 1) {
+	            return false;
+	        }
+	        i = 1;
+	    }
+	    for (; i < str.length(); i++) {
+	        char c = str.charAt(i);
+	        if (c < '0' || c > '9') {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	
 }
