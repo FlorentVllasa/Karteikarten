68505a70452bb7e2e2acd35f9bc70dfe7b86dd4d
Florent <florentus998@hotmail.com>
2019-03-29 16:03:52 +0100

    Gui Update, unnecessary Controller Classes and fxml files deleted, MenuItems are done through code

--- karteikarten/src/main/java/de/kksystem/karteikarten/view/javafx/controllers/CreateWindowController.java
+++ karteikarten/src/main/java/de/kksystem/karteikarten/view/javafx/controllers/CreateWindowController.java
@@ -4,13 +4,13 @@ import de.kksystem.karteikarten.view.javafx.helperclasses.WindowPresetSwitchStag
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class CreateWindowController implements Initializable {
@@ -27,24 +27,168 @@ public class CreateWindowController implements Initializable {
     private Button btnSave;
 
     @FXML
-    private MenuItem addCategoryMenuItem;
+    private MenuItem miAddCategory;
 
     @FXML
-    private MenuItem editCategoryMenuItem;
+    private MenuItem miEditCategory;
 
     @FXML
-    private MenuItem deleteCategoryMenuItem;
+    private MenuItem miDeleteCategory;
 
-    public void switchToAddCategoryWindow(ActionEvent event){
-        switchStageHelper.createWindowNewStage("/fxml/createCategoryWindow.fxml", "Kategorie erstellen!", new CreateCategoryController());
+    @FXML
+    private MenuItem miAddLection;
+
+    @FXML
+    private MenuItem miEditLection;
+
+    @FXML
+    private MenuItem miDeleteLection;
+
+    @FXML
+    private MenuItem miEditIndexCard;
+
+    @FXML
+    private MenuItem miDeleteIndexCard;
+
+    @FXML
+    private MenuItem miEditFavoritelist;
+
+    @FXML
+    private MenuItem miDeleteFavoritelist;
+
+    public void switchToAddCategoryDialog(ActionEvent event){
+        TextInputDialog tid = new TextInputDialog("Kategorienamen eingeben");
+        tid.setTitle("Kategorie erstellen");
+        tid.setHeaderText(null);
+        tid.setContentText("Bitte Kategorienamen eingeben: ");
+
+        Optional<String> enteredValue = tid.showAndWait();
+
+        if(enteredValue.get().isBlank()) {
+            System.out.println("Unknown");
+        }else {
+            System.out.println(enteredValue.get());
+        }
     }
 
-    public void switchToEditCategoryWindow(ActionEvent event){
-        switchStageHelper.createWindowNewStage("/fxml/editCategoryWindow.fxml", "Kategorie bearbeiten!", new EditCategoryController());
+    public void switchToEditCategoryDialog(ActionEvent event){
+        TextInputDialog tid = new TextInputDialog("Neuer Kategoriename");
+        tid.setTitle("Kategorie bearbeiten");
+        tid.setHeaderText(null);
+        tid.setContentText("Bitten den neuen Kategorienamen eingeben");
+
+        Optional<String> enteredValue = tid.showAndWait();
+
+        if(enteredValue.get().isBlank()) {
+            System.out.println("Unknown");
+        }else {
+            System.out.println(enteredValue.get());
+        }
     }
 
-    public void switchToDeleteCategoryWindow(ActionEvent event){
-        switchStageHelper.createWindowNewStage("/fxml/deleteCategoryWindow.fxml", "Kategorie loeschen!", new DeleteCategoryController());
+    public void switchToDeleteCategoryDialog(ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Kategorie loeschen!");
+        alert.setHeaderText("Achtung!");
+        alert.setContentText("Soll endgueltig geloescht werden?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK, hier wird gelöscht
+        } else {
+
+        }
+    }
+
+    public void switchtoAddLectionDialog(ActionEvent event){
+        TextInputDialog tid = new TextInputDialog("Lektionnamen eingeben");
+        tid.setTitle("Lektion erstellen");
+        tid.setHeaderText(null);
+        tid.setContentText("Bitte Lektionnamen eingeben:");
+
+        Optional<String> enteredValue = tid.showAndWait();
+
+        if(enteredValue.get().isBlank()) {
+            System.out.println("Unknown");
+        }else {
+            System.out.println(enteredValue.get());
+        }
+    }
+
+    public void switchToEditLectionDialog(ActionEvent event){
+        TextInputDialog tid = new TextInputDialog("Neuer Lektionname");
+        tid.setTitle("Lektion bearbeiten");
+        tid.setHeaderText(null);
+        tid.setContentText("Bitte neuen Lektionnamen eingeben:");
+
+        Optional<String> enteredValue = tid.showAndWait();
+
+        if(enteredValue.get().isBlank()) {
+            System.out.println("Unknown");
+        }else {
+            System.out.println(enteredValue.get());
+        }
+    }
+
+    public void switchToDeleteLectionDialog(ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Lektion loeschen!");
+        alert.setHeaderText("Achtung!");
+        alert.setContentText("Soll endgueltig geloescht werden?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK, hier wird gelöscht
+        } else {
+
+        }
+    }
+
+    public void switchToEditIndexCardDialog(ActionEvent event){
+        switchStageHelper.createWindowNewStage("/fxml/editIndexCardWindow.fxml", "Karteikarte bearbeiten!", new EditIndexCardController());
+    }
+
+    public void switchToDeleteIndexCardDialog(ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Karteikarte loeschen!");
+        alert.setHeaderText("Achtung!");
+        alert.setContentText("Soll endgueltig geloescht werden?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK, hier wird gelöscht
+        } else {
+
+        }
+    }
+
+    public void switchtoEditFavoritelistDialog(ActionEvent event){
+        TextInputDialog tid = new TextInputDialog("Neuer Favoritenname");
+        tid.setTitle("Favoritenliste bearbeiten");
+        tid.setHeaderText(null);
+        tid.setContentText("Bitte neuen Favoritennamen eingeben:");
+
+        Optional<String> enteredValue = tid.showAndWait();
+
+        if(enteredValue.get().isBlank()) {
+            System.out.println("Unknown");
+        }else {
+            System.out.println(enteredValue.get());
+        }
+    }
+
+    public void switchToDeleteFavoritelistDialog(ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Favorit loeschen!");
+        alert.setHeaderText("Achtung!");
+        alert.setContentText("Soll endgueltig geloescht werden?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK, hier wird gelöscht
+        } else {
+
+        }
     }
 
     /*Diese Methode nimmt sich die Stage Information der Scene und schließt das Fenster daraufhin.
@@ -59,8 +203,15 @@ public class CreateWindowController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         btnBack.setOnAction(this::closeCreateWindow);
-        addCategoryMenuItem.setOnAction(this::switchToAddCategoryWindow);
-        editCategoryMenuItem.setOnAction(this::switchToEditCategoryWindow);
-        deleteCategoryMenuItem.setOnAction(this::switchToDeleteCategoryWindow);
+        miAddCategory.setOnAction(this::switchToAddCategoryDialog);
+        miEditCategory.setOnAction(this::switchToEditCategoryDialog);
+        miDeleteCategory.setOnAction(this::switchToDeleteCategoryDialog);
+        miAddLection.setOnAction(this::switchtoAddLectionDialog);
+        miEditLection.setOnAction(this::switchToEditLectionDialog);
+        miDeleteLection.setOnAction(this::switchToDeleteLectionDialog);
+        miEditIndexCard.setOnAction(this::switchToEditIndexCardDialog);
+        miDeleteIndexCard.setOnAction(this::switchToDeleteIndexCardDialog);
+        miEditFavoritelist.setOnAction(this::switchtoEditFavoritelistDialog);
+        miDeleteFavoritelist.setOnAction(this::switchToDeleteFavoritelistDialog);
     }
 }
---
+++ karteikarten/src/main/java/de/kksystem/karteikarten/view/javafx/controllers/EditIndexCardController.java
@@ -0,0 +1,17 @@
+package de.kksystem.karteikarten.view.javafx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class EditIndexCardController {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnDeletePicture;
+
+    @FXML
+    private Button btnSave;
+
+}
