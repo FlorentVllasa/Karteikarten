dac00e92f281095c7814a3097c349a9032b6d198
IgnatiusJuanPradipta <IgnatiusJuanPradipta@192.168.0.51>
2019-04-03 23:41:29 +0200

    add time in loginwindow

--- karteikarten/src/main/java/de/kksystem/karteikarten/view/javafx/controllers/LoginController.java
+++ karteikarten/src/main/java/de/kksystem/karteikarten/view/javafx/controllers/LoginController.java
@@ -2,6 +2,8 @@ package de.kksystem.karteikarten.view.javafx.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 import de.kksystem.karteikarten.data.UserData;
@@ -12,6 +14,7 @@ import de.kksystem.karteikarten.model.interfaces.User;
 import de.kksystem.karteikarten.view.javafx.helperclasses.WindowPresetSwitchScene;
 import de.kksystem.karteikarten.view.javafx.helperclasses.WindowPresetSwitchStage;
 import de.kksystem.karteikarten.view.javafx.stages.LoginWindow;
+import javafx.animation.Animation;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -59,6 +62,8 @@ public class LoginController implements Initializable {
     @FXML
     private Label lblLogInMessage;
     
+    @FXML
+    private Label lblDateTime;
     
     @FXML
     private void logIn(ActionEvent event) {
@@ -139,10 +144,20 @@ public class LoginController implements Initializable {
     private void onEnterPassword(ActionEvent event) {
     	logIn(event);
     }
+    
+    private void showTime() {
+    	Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            lblDateTime.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
 
     /*Hier werden die anklickbaren Button ihren jeweiligen Methoden zugewiesen*/
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    	showTime();
         buttonLogin.setOnAction(this::logIn);
         buttonForgotPassword.setOnAction(this::switchToForgotPasswordWindow);
         buttonRegister.setOnAction(this::switchToRegistrationWindow);
