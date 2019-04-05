fd4d3c9f1c33b81c18a17dfad257e2e8adc3b93f
IgnatiusJuanPradipta <IgnatiusJuanPradipta@192.168.0.118>
2019-04-05 14:02:27 +0200

    learnWindow fertig

--- karteikarten/src/main/java/de/kksystem/karteikarten/view/javafx/controllers/LearnWindowController.java
+++ karteikarten/src/main/java/de/kksystem/karteikarten/view/javafx/controllers/LearnWindowController.java
@@ -1,57 +1,73 @@
 package de.kksystem.karteikarten.view.javafx.controllers;
 
+import de.kksystem.karteikarten.dao.classes.jdbc.LectionDaoJdbcImpl;
+import de.kksystem.karteikarten.dao.classes.jdbc.UserDaoJdbcImpl;
+import de.kksystem.karteikarten.dao.interfaces.UserDao;
 import de.kksystem.karteikarten.data.UserData;
 import de.kksystem.karteikarten.facades.ServiceFacade;
+import de.kksystem.karteikarten.model.classes.LectionImpl;
 import de.kksystem.karteikarten.model.interfaces.IndexCard;
-
-import java.sql.ResultSet;
+import de.kksystem.karteikarten.model.interfaces.Lection;
 import java.util.List;
-import java.lang.String;
-
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import org.sqlite.JDBC;
 
 import java.net.URL;
+import java.text.MessageFormat;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class LearnWindowController implements Initializable {
-
-    ObservableList<Lection> list = FXCollections.observableArrayList();
 
 	@FXML
     private SplitPane splitPane;
 
     @FXML
-    private TextField textFieldQuestion;
+    private WebView webFieldQuestion;
 
     @FXML
-    private TextField textFieldAnswer;
+    private WebView webFieldAnswer;
 
     @FXML
-    private Button btnBack;
+    private Button btnPrevious;
 
     @FXML
-    private Button btnAnswer;
+    private Button btnShowAnswer;
 
     @FXML
-    private Button btnContinue;
+    private Button btnNext;
 
     @FXML
     private Button btnCloseWindow;
-    private Object Lection;
 
     private List<IndexCard> cards;
+    
+    private List<IndexCard> wronganswerCards;
 
+    private IntegerProperty currentIndex;
+    private IntegerProperty minIndex;
+    private IntegerProperty maxIndex;
+    
+
+    private void getCards(){
+        int lektionId = UserData.getInstance().getLection().getLectionId();
+        cards = ServiceFacade.getInstance().findAllIndexCardsByLectionId(lektionId);
+    }
+    
     /*Diese Methode nimmt sich die Stage Information der Scene und schließt das Fenster daraufhin.
      * Dies wird erreicht in dem die Stage Information irgendeiner Komponente der Scene ermittelt wird und
      * dann in ein Stage Objekt umgewandelt wird.*/
@@ -59,61 +75,50 @@ public class LearnWindowController implements Initializable {
         Stage stageInfo = (Stage) splitPane.getScene().getWindow();
         stageInfo.close();
     }
-
-    private void btnAnswer(ActionEvent event){
-        UserData.getInstance().getLection().getLectionId();
-
+    
+    private void showQuestion(int cardIndex) {
+    	webFieldQuestion.getEngine().loadContent(cards.get(cardIndex).getQuestion());
+    }
+    
+    private void showAnswer(int cardIndex) {
+    	webFieldAnswer.getEngine().loadContent(cards.get(cardIndex).getAnswer());
     }
 
-    private void getCards(){
-        int lektionId = UserData.getInstance().getLection().getLectionId();
-        cards = ServiceFacade.getInstance().findAllIndexCardsByLectionId(lektionId);
-
-
+    private void showAnswer(ActionEvent event){
+    	showAnswer(currentIndex.getValue());
+    }
+    
+    private void prevQuestion(ActionEvent event) {
+    	currentIndex.set(currentIndex.getValue() - 1);
+    	showQuestion(currentIndex.getValue());
+    	webFieldAnswer.getEngine().loadContent("");
+    }
+    
+    private void nextQuestion(ActionEvent event) {
+    	currentIndex.set(currentIndex.getValue() + 1);
+    	showQuestion(currentIndex.getValue());
+    	webFieldAnswer.getEngine().loadContent("");
     }
 
+    private void setDisableProperty() {
+    	btnPrevious.disableProperty().bind(currentIndex.isEqualTo(minIndex));
+    	btnNext.disableProperty().bind(currentIndex.isEqualTo(maxIndex));
+    }
+    
     /*Hier werden die anklickbaren Button ihren jeweiligen Methoden zugewiesen*/
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         getCards();
+        currentIndex = new SimpleIntegerProperty(this, "currentIndex", 0);
+        minIndex = new SimpleIntegerProperty(this, "minIndex", 0);
+        maxIndex = new SimpleIntegerProperty(this, "currentIndex", cards.size()-1);
+        setDisableProperty();
+        
+        btnShowAnswer.setOnAction(this::showAnswer);
+        btnPrevious.setOnAction(this::prevQuestion);
+        btnNext.setOnAction(this::nextQuestion);
         btnCloseWindow.setOnAction(this::closeLearnWindow);
-        initTextField();
-
-
-        btnAnswer();
-
-
-    }
-
-    private void btnAnswer() {
-        UserData handler = new UserData();
-        String ho = "SELECT LektionID FROM Lektion";
-        ResultSet rs;
-        rs = handler.setLection(ho);
-        try{
-          while(rs.next()){
-              String answer = rs.getString("answer");
-                list.add(new Lection(answer));
-            }
-        } catch (Exception ex){
-
-        }
-    }
-
-    private void initTextField() {
-        //textFieldAnswer.(new PropertyValueFactory<Lection, String>("answer"));
-
-    }
-
-    public static class Lection{
-        private final SimpleStringProperty answer;
-
-         Lection(String answer){
-             this.answer = new SimpleStringProperty(answer);
-         }
-
-         public String getAnswer(){
-             return answer.get();
-         }
+        
+        showQuestion(currentIndex.getValue());
     }
 }
